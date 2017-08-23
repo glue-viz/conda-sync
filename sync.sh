db4d6ef2d270d2ab9fc79819fe7cc2a6a3bfb22a
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-23 14:42:15 +0100

    Don't copy over glue-* packages but copy over rasterio

--- sync.sh
+++ sync.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-python sync.py --package glue-core --source conda-forge --destination glueviz
-python sync.py --package glue-vispy-viewers --source conda-forge --destination glueviz
-python sync.py --package glueviz --source conda-forge --destination glueviz
+# We now build the following ourselves, so don't need to copy them over
+# python sync.py --package glue-core --source conda-forge --destination glueviz
+# python sync.py --package glue-vispy-viewers --source conda-forge --destination glueviz
+# python sync.py --package glueviz --source conda-forge --destination glueviz
 
-
+# The following packages are needed by glue-geospatial and are out of date in defaults
+python sync.py --package rasterio --source conda-forge --destination glueviz
