bfd942ba1501d33ccdf3d35ce955b7c86fe1f0d8
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-24 12:40:14 +0100

    Added fast-histogram and mpl-scatter-density

--- sync.sh
+++ sync.sh
@@ -5,6 +5,10 @@
 # python sync.py --package glue-vispy-viewers --source conda-forge --destination glueviz
 # python sync.py --package glueviz --source conda-forge --destination glueviz
 
+python sync.py --package fast-histogram --source conda-forge --destination fast-histogram
+python sync.py --package mpl-scatter-density --source conda-forge --destination mpl-scatter-density
+
 # The following packages are needed by glue-geospatial and are out of date in defaults
 python sync.py --package rasterio --source conda-forge --destination glueviz
 python sync.py --package snuggs --source conda-forge --destination glueviz
+
